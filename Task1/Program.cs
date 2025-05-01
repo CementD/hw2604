47e6b12c9f35a488719211e8c4810956558c1449
Semen Domin <deminsem2008@gmail.com>
2025-05-01 16:21:55 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.Sockets;
+
+namespace Task1
+{
+    class Program
+    {
+        static int goldMine = 100;
+        static object locker = new object();
+        static bool collapse = false;
+
+        static void Main()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+
+            List<Task> unitTasks = new List<Task>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int unitId = i + 1;
+                Task task = Task.Run(() => UnitWork(unitId, token));
+                unitTasks.Add(task);
+            }
+
+            Thread.Sleep(5000);
+            Console.WriteLine("\nMine collapse! Units come back to camp...\n");
+            collapse = true;
+
+            Thread.Sleep(5000);
+            collapse = false;
+            Console.WriteLine("\nMine is safe! Units come back to work...\n");
+
+            foreach (Task task in unitTasks)
+            {
+                task.Wait();
+            }
+
+            Console.WriteLine("\nMining is finished!");
+            cancellationTokenSource.Cancel();
+            Console.ReadKey();
+        }
+
+        static void UnitWork(int id, CancellationToken token)
+        {
+            Random rnd = new Random();
+
+            while (!token.IsCancellationRequested)
+            {
+                if (collapse)
+                {
+                    Console.WriteLine($"[Unit {id}] Collapse! Wait...");
+                    Thread.Sleep(5000);
+                    continue;
+                }
+
+                Thread.Sleep(rnd.Next(500, 1500));
+
+                int mined = 0;
+
+                lock (locker)
+                {
+                    if (goldMine <= 0)
+                        return;
+
+                    mined = rnd.Next(1, 5);
+                    if (goldMine - mined < 0)
+                        mined = goldMine;
+
+                    goldMine -= mined;
+
+                    Console.WriteLine($"[Unit {id}] Mined {mined} of gold. Last: {goldMine}");
+
+                    if (goldMine <= 0)
+                        return;
+                }
+            }
+        }
+    }
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static int[] teams = new int[3];
+        static object locker = new object();
+        static bool ceasefire = false;
+        
+
+        static void Main()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 0; i < teams.Length; i++)
+            {
+                int teamId = i;
+                teams[teamId] = new Random().Next(10, 20);
+                tasks.Add(Task.Run(() => Fight(teamId, token)));
+            }
+
+            Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Thread.Sleep(7000);
+                    ceasefire = true;
+                    Console.WriteLine("\nCeasefire for 3 seconds...\n");
+                    Thread.Sleep(3000);
+                    ceasefire = false;
+                    Console.WriteLine("\nFigth was restarted!\n");
+                }
+            });
+
+            Thread.Sleep(35000);
+            cancellationTokenSource.Cancel();
+
+            Console.WriteLine("\nEnd of the game. Result:\n");
+
+            foreach (Task t in tasks)
+            {
+                t.Wait();
+            }
+
+            for (int i = 0; i < teams.Length; i++)
+            {
+                Console.WriteLine($"Team {i + 1}: {teams[i]} fighters");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Fight(int teamId, CancellationToken token)
+        {
+            Random rnd = new Random();
+
+            while (!token.IsCancellationRequested)
+            {
+                if (ceasefire)
+                {
+                    Thread.Sleep(500);
+                    continue;
+                }
+
+                Thread.Sleep(rnd.Next(1000, 2000));
+
+                lock (locker)
+                {
+                    int recruits = rnd.Next(1, 5);
+                    teams[teamId] += recruits;
+
+                    int targetId;
+                    do
+                    {
+                        targetId = rnd.Next(0, teams.Length);
+                    } while (targetId == teamId);
+
+                    int damage = rnd.Next(1, 5);
+                    teams[targetId] -= damage;
+                    if (teams[targetId] < 0)
+                        teams[targetId] = 0;
+
+                    Console.WriteLine($"[Team {teamId + 1}] +{recruits} fighters | Atack -> Team {targetId + 1}, -{damage}. Status: [{string.Join(", ", teams)}]");
+                }
+            }
+        }
+    }
+}
